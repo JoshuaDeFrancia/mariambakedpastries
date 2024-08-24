da950ee65f2a67fd88fc353794b7a50d849e1e83
kotaiai <91441140+kotaiai@users.noreply.github.com>
2024-08-24 21:25:27 +0800

    Update script.js
    Binaliktad ko position ng toggle kaya pati to binaliktad ko

--- Codes/script.js
+++ Codes/script.js
@@ -3,9 +3,9 @@ const account_signup_btn = document.getElementById('account_signup_btn');
 const account_login_btn = document.getElementById('account_login_btn');
 
 account_signup_btn.addEventListener('click', () =>{
-    account_container.classList.remove("active");
+    account_container.classList.add("active");
 });
 
 account_login_btn.addEventListener('click', () =>{
-    account_container.classList.add("active");
+    account_container.classList.remove("active");
 });
