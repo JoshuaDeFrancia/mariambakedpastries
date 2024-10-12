59c8def2e086e9022c1200df397700d24abb55eb
JoshuaDeFrancia <152280820+JoshuaDeFrancia@users.noreply.github.com>
2024-10-12 14:59:16 +0800

    Update script.js

--- Codes/script.js
+++ Codes/script.js
@@ -91,5 +91,56 @@ function signup_reveal_password2() {
 }
 
 
+<!-- SCRIPT NG POP UP -->
+    <script>
+        function showPopup() {
+            document.getElementById('product-popup').style.display = 'block';
+            document.getElementById('backdrop').style.display = 'block'; 
+        }
+
+        function closePopup() {
+            document.getElementById('product-popup').style.display = 'none';
+            document.getElementById('backdrop').style.display = 'none'; 
+        }
+
+        // SCRIPT NG DECREASE AND INCREASE BUTTON SA POP UP
+        function increaseQuantity() {
+            var quantity = document.getElementById('order-quantity').innerText; 
+            var newQuantity = parseInt(quantity) + 1; 
+            document.getElementById('order-quantity').innerText = newQuantity; 
+        }
+
+        function decreaseQuantity() {
+            var quantity = document.getElementById('order-quantity').innerText; 
+            var newQuantity = Math.max(parseInt(quantity) - 1, 0); 
+            document.getElementById('order-quantity').innerText = newQuantity; 
+        }
+
+        function selectQuantity(type) {
+            const buttons = document.querySelectorAll('.quantity-btn');
+            buttons.forEach(button => {
+                button.classList.remove('selected'); 
+            });
+
+            if (type === 'half') {
+                buttons[0].classList.add('selected');
+            } else if (type === 'dozen') {
+                buttons[1].classList.add('selected');
+            }
+        }
+
+        const checkboxes = document.querySelectorAll('.category-checkbox');
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                    checkboxes.forEach(cb => {
+                        if (cb !== this) {
+                            cb.checked = false;
+                        }
+                    });
+                }
+            });
+        });
+    </script>
 
 
